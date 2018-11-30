3d44811b44f18a4919937916fc0fb89013d95f7c
zglossip <zglossip@oreillyauto.com>
2018-11-30 10:43:56 -0600

    Initial commit

---
+++ src/main/components/App.js
@@ -0,0 +1,13 @@
+import React, {PureComponent} from 'react';
+
+class App extends PureComponent{
+  render() {
+    return (
+      <div>
+        <span>Squares!</span>
+      </div>
+    );
+  }
+}
+
+export default App;
