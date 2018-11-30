dbc90638168fe3269d77f917e6e0a22e711b0d64
zglossip <zglossip@oreillyauto.com>
2018-11-30 11:06:50 -0600

    Fix webpack error

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 const htmlPlugin = new HtmlWebPackPlugin({
-  template: './src/main/index.html',
+  template: './src/index.html',
   filename: './index.html'
 });
 
